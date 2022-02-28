5e30a92ac9d2d1c0c6d812acc9c75673393979cb
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-02-28 11:47:45 -0500

    about title and headshot aligned

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './about.css'
+import headshot from '../../assets/headshot.jpg'
 
 function About() {
   return (
     <div className='about' id='about'>
+     <div className='left'>
+       <div className='imgContainer'>
+         <img src={require('../../assets/headshot.jpg')} alt=''/>
+       </div>
+     </div>
+     <div className='right'>
+       <div className='wrapper'>
+         <h2>Hello World! I'm</h2>
+         <h1>Jessica Jeyanthiran</h1>
+         <h3>Full Stack Web Developer</h3>
+       </div>
+
+     </div>
 
     </div>
   )
