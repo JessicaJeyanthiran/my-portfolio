fe50cd2724c4ab300e1cf4243510c453c2db994e
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-02-28 18:24:43 -0500

    contact me page styling

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './contact.css'
+import contact from "../../assets/contactme.jpg"
 
 function Contact() {
+
+  const [message,setMessage] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  }
+
   return (
     <div className='contact' id='contact'>
+      <div className='left'>
+        <h2> Contact Me.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email"></input>
+          <textarea placeholder='Message'></textarea>
+          <button type='submit'>Send</button>
+          {message && <span> Thanks! I'll reply ASAP</span>}
+        </form>
+      </div>
+      <div className='right'>
+        
+      </div>
 
     </div>
   )
