0d45eac3bcc3e91c55e72e700252c76cf575c0d8
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-03-01 22:25:03 -0500

    modal added to learn more button

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './about.css'
 import headshot from '../../assets/headshot2.png';
 import Resume from '../../assets/JessicaJeyanthiranFullStackResume.pdf';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { spacing } from '@mui/system';
 import { touchRippleClasses } from '@mui/material';
+import Modal from '../modal';
 
 function About() {
+
+  const[openModal, setOpenModal] = useState(false);
 
   // const buttons = document.querySelectorAll('a');
   // buttons.forEach(button => {
@@ -15,15 +18,17 @@ function About() {
   //     let y = e.clientY - e.target.offsetTop;
 
   //     let ripples = document.createElement ('span');
-  //     touchRippleClasses.setAttribute('className', 'span');
   //     ripples.style.left = x + 'px';
   //     ripples.style.top = y + 'px';
   //     this.appendChild(ripples);
+  //     ripples.className='span';
   //   })
   // })
   
   return (
     <div className='about' id='about'>
+      {openModal && <Modal closeModal={setOpenModal}/>}
+
      <div className='left'>
        <div className='imgContainer'>
          <img src={require('../../assets/headshot2.png')} alt=''/>
@@ -34,12 +39,22 @@ function About() {
           <h2>Hello World! I'm</h2>
           <h1>Jessica Jeyanthiran</h1>
           <h3>Full Stack Web Developer</h3>
-        </div>
-        <button id='button'>Learn More!</button><a href={Resume} download><button id='button'>Download Resume</button></a>
 
-         <a href="#portfolio">
+        </div>
+       
+
+        <button 
+          className='openModalBtn' 
+          onClick={() => {
+            setOpenModal(true);
+        }}>
+          Learn More!
+        </button>
+
+        <a href={Resume} download><button id='button'>Download Resume</button></a>
+        <a href="#portfolio">
          <KeyboardArrowDownIcon className='arrow'/>
-       </a>
+        </a>
       </div>
         
          
