f28459424eab5fa168250e955c0bac96f2371b0f
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-03-01 20:45:23 -0500

    attempt to give ripples to buttons, commented out

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,8 +3,25 @@ import './about.css'
 import headshot from '../../assets/headshot2.png';
 import Resume from '../../assets/JessicaJeyanthiranFullStackResume.pdf';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { spacing } from '@mui/system';
+import { touchRippleClasses } from '@mui/material';
 
 function About() {
+
+  // const buttons = document.querySelectorAll('a');
+  // buttons.forEach(button => {
+  //   button.addEventListener('click', function(e){
+  //     let x = e.clientX - e.target.offsetLeft;
+  //     let y = e.clientY - e.target.offsetTop;
+
+  //     let ripples = document.createElement ('span');
+  //     touchRippleClasses.setAttribute('className', 'span');
+  //     ripples.style.left = x + 'px';
+  //     ripples.style.top = y + 'px';
+  //     this.appendChild(ripples);
+  //   })
+  // })
+  
   return (
     <div className='about' id='about'>
      <div className='left'>
@@ -18,7 +35,7 @@ function About() {
           <h1>Jessica Jeyanthiran</h1>
           <h3>Full Stack Web Developer</h3>
         </div>
-        <button className='button1'>Learn More!</button><a href={Resume} download><button className='button2 p'>Download Resume</button></a>
+        <button id='button'>Learn More!</button><a href={Resume} download><button id='button'>Download Resume</button></a>
 
          <a href="#portfolio">
          <KeyboardArrowDownIcon className='arrow'/>
@@ -32,7 +49,11 @@ function About() {
      
 
     </div>
+
+    
   )
+
+
 };
   
   export default About;
