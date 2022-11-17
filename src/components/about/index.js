d8ee11a6cf4382d04e1fb1d4ba772aa3dd8d2a26
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-11-17 03:06:37 -0500

    hero background animation

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -27,14 +27,29 @@ function About() {
      </div> */}
     
         <div className='text-center p-5'>
-          <h2 className="text-4xl py-2 text-slate-600 font-medium font-burtons" >Jessica Jeyanthiran</h2>
+
+        
+
+          <h2 className="text-4xl py-4 text-slate-600 font-medium font-burtons" >Jessica Jeyanthiran</h2>
           <h3 className='text-xl py-2'>Full Stack Web Developer.</h3>
-          <p className="text-sm py-3 leading-8 text-slate-800">I am a full-stack web developer with a certificate earned from the University of Toronto. Equipped me with front end skills in HTML, CSS, Javascript, React.js, and responsive web development, as well as back end technologies such as Node. js, Express.js, MongoDB, MySQL.</p></div>
+          <p className="text-sm py-3 leading-8 text-slate-800">I am a full-stack web developer with a certificate earned from the University of Toronto. Equipped with front end skills in HTML, CSS, Javascript, React.js, and responsive web development, as well as back end technologies such as Node. js, Express.js, MongoDB, MySQL.</p>
+        </div>
         <div className="text-3xl flex justify-center gap-16 py-2 text-slate-600 drop-shadow-lg">
           <AiFillLinkedin/>
           <AiFillGithub/>
           <AiFillMail/>
         </div>
+
+        <div className="flex justify-center"> 
+          <div className='absolute top-60 left-96 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-80 animate-blob animation-delay-2000'>
+          </div>
+          <div className='absolute top-60 right-96 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-80 animate-blob'>
+          </div>
+          <div className='absolute top-80 -bottom-60 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-80 animate-blob animation-delay-4000'>
+          </div>
+        </div>
+
+
         <div className="mx-auto rounded-full relative mt-5 mb-10 md:h-50 md:w-60 drop-shadow-xl">
         <img src={require('../../assets/headshot2.png')} alt=''/>
         </div>
